48554467c5475422dc437d8bd5d3d0e0a6cd7772
jonathan.kanarek <jonathan.kanarek@kaltura.com>
2017-08-06 17:17:25 +0300

    debug msg

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,12 @@
 travis_fold() {
   local action=$1
   local name=$2
+  echo "travis fold: ${action}: ${name}"
   echo -en "travis_fold:${action}:${name}\r"
 }
 
-travis_fold start foo
-
-echo "xcodebuild"
+travis_fold start xcodebuild
 
 xcodebuild clean build test -workspace Example/KalturaClient.xcworkspace -scheme KalturaClient_Tests -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1'
 
-travis_fold end foo
+travis_fold end xcodebuild
