559929ea6afcb5a5e101816fdf2f0edf54e6ed49
Jess Portnoy <jess.portnoy@kaltura.com>
2017-08-21 19:18:05 +0100

    The output from xcodebuild is too long, resulting in "The log length has exceeded the limit of 4 MB (this usually means that the test suite is raising the same exception over and over" If you redirect all of it to a file, Travis complains about "No output has been received in the last 10m0s" and terminates the build.
    travis_build.sh will output the last 100 lines of the log every 30
    seconds to avoid the "No output recieved" issue and once the build is
    done, pass the full log through xcpretty so the important bits can be
    properly outputted for debugging purposes.

---
+++ travis_build.sh
@@ -0,0 +1,33 @@
+# Abort on Error
+set -e
+
+SLEEP=30s
+BUILD_OUTPUT=/tmp/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 500 lines of output:
+   cat $BUILD_OUTPUT | xcpretty 
+}
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+
+bash -c "while true; do tail -100 $BUILD_OUTPUT; sleep $SLEEP; done" &
+PING_LOOP_PID=$!
+
+xcodebuild clean build test -workspace Example/KalturaClient.xcworkspace -scheme KalturaClient_Tests -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1' $BUILD_OUTPUT> 2>&1
+
+
+# The build finished without returning an error so dump a tail of the output
+dump_output
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
