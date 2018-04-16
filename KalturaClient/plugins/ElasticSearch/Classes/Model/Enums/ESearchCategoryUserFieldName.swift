f1052726c4c0089b76eb33b8e1280623968e27e6
Kaltura CSI <community@kaltura.com>
2018-04-16 01:33:22 -0400

    New clientlib ver

--- KalturaClient/plugins/ElasticSearch/Classes/Model/Enums/ESearchCategoryUserFieldName.swift
+++ KalturaClient/plugins/ElasticSearch/Classes/Model/Enums/ESearchCategoryUserFieldName.swift
@@ -31,31 +31,6 @@
  * 
  * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
  */
-public enum ESearchCategoryFieldName: String {
-	case CONTRIBUTION_POLICY = "contribution_policy"
-	case CREATED_AT = "created_at"
-	case DEPTH = "depth"
-	case DESCRIPTION = "description"
-	case DIRECT_ENTRIES_COUNT = "direct_entries_count"
-	case DIRECT_SUB_CATEGORIES_COUNT = "direct_sub_categories_count"
-	case DISPLAY_IN_SEARCH = "display_in_search"
-	case ENTRIES_COUNT = "entries_count"
-	case FULL_IDS = "full_ids"
-	case FULL_NAME = "full_name"
-	case ID = "id"
-	case INHERITANCE_TYPE = "inheritance_type"
-	case INHERITED_PARENT_ID = "inherited_parent_id"
-	case MEMBERS_COUNT = "members_count"
-	case MODERATION = "moderation"
-	case NAME = "name"
-	case PARENT_ID = "parent_id"
-	case PENDING_ENTRIES_COUNT = "pending_entries_count"
-	case PENDING_MEMBERS_COUNT = "pending_members_count"
-	case PRIVACY = "privacy"
-	case PRIVACY_CONTEXT = "privacy_context"
-	case PRIVACY_CONTEXTS = "privacy_contexts"
-	case REFERENCE_ID = "reference_id"
-	case TAGS = "tags"
-	case UPDATED_AT = "updated_at"
+public enum ESearchCategoryUserFieldName: String {
 	case USER_ID = "user_id"
 }
