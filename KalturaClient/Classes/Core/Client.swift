e990de52a6f8719335ef9799e6ddbebc13a037ed
Kaltura CSI <community@kaltura.com>
2018-10-04 02:45:25 -0400

    New clientlib ver

--- KalturaClient/Classes/Core/Client.swift
+++ KalturaClient/Classes/Core/Client.swift
@@ -40,7 +40,7 @@
 		
 		super.init()
 		
-		clientTag = "swift:18-10-03"
+		clientTag = "swift:18-10-04"
 		apiVersion = "14.6.0"
 	}
 }
--- KalturaClient/Classes/Services/GroupUserService.swift
+++ KalturaClient/Classes/Services/GroupUserService.swift
@@ -102,4 +102,50 @@ public final class GroupUserService{
 
 		return request
 	}
+
+	public class SyncTokenizer: ClientTokenizer  {
+		
+		public var userId: BaseTokenizedObject {
+			get {
+				return self.append("userId") 
+			}
+		}
+		
+		public var groupIds: BaseTokenizedObject {
+			get {
+				return self.append("groupIds") 
+			}
+		}
+		
+		public var removeFromExistingGroups: BaseTokenizedObject {
+			get {
+				return self.append("removeFromExistingGroups") 
+			}
+		}
+		
+		public var createNewGroups: BaseTokenizedObject {
+			get {
+				return self.append("createNewGroups") 
+			}
+		}
+	}
+
+	public static func sync(userId: String, groupIds: String) -> RequestBuilder<BulkUpload, BulkUpload.BulkUploadTokenizer, SyncTokenizer> {
+		return sync(userId: userId, groupIds: groupIds, removeFromExistingGroups: true)
+	}
+
+	public static func sync(userId: String, groupIds: String, removeFromExistingGroups: Bool?) -> RequestBuilder<BulkUpload, BulkUpload.BulkUploadTokenizer, SyncTokenizer> {
+		return sync(userId: userId, groupIds: groupIds, removeFromExistingGroups: removeFromExistingGroups, createNewGroups: true)
+	}
+
+	/**  sync by userId and groupIds  */
+	public static func sync(userId: String, groupIds: String, removeFromExistingGroups: Bool?, createNewGroups: Bool?) -> RequestBuilder<BulkUpload, BulkUpload.BulkUploadTokenizer, SyncTokenizer> {
+		let request: RequestBuilder<BulkUpload, BulkUpload.BulkUploadTokenizer, SyncTokenizer> = RequestBuilder<BulkUpload, BulkUpload.BulkUploadTokenizer, SyncTokenizer>(service: "groupuser", action: "sync")
+			.setParam(key: "userId", value: userId)
+			.setParam(key: "groupIds", value: groupIds)
+			.setParam(key: "removeFromExistingGroups", value: removeFromExistingGroups)
+			.setParam(key: "createNewGroups", value: createNewGroups)
+
+		return request
+	}
 }
